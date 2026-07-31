e8466ea076cb085d912e8f9fc1f3a0c14980afe7
zakyrmh <zaxxyyramadhan@gmail.com>
2026-07-31 17:55:32 +0700

    feat: register application policies, event listeners, custom middleware, and HTTPS proxy configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO |
+                Request::HEADER_X_FORWARDED_AWS_ELB
+        );
         // Daftarkan alias 'role' untuk RoleMiddleware
         $middleware->alias([
             'role' => RoleMiddleware::class,
